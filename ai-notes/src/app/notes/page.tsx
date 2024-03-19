f3dfc002ccf05301732df054a20b04d3a1a96a0a
Gavin Yip <gavin.yip88@gmail.com>
2024-03-19 13:34:10 +0000

    rendered noted out

--- ai-notes/src/app/notes/page.tsx
+++ ai-notes/src/app/notes/page.tsx
@@ -1,3 +1,4 @@
+import Note from "@/components/ui/Note";
 import prisma from "@/lib/db/prisma";
 import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
@@ -13,5 +14,19 @@ export default async function NotesPage() {
   }
   const allNotes = await prisma.note.findMany({ where: { userId } });
 
-  return <div>{JSON.stringify(allNotes)}</div>;
+  return (
+    //map over notes and display them
+    //import and render the Note component
+    <div className="mx-14 my-8 grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+      {/* if no notes are found, display a message */}
+      {allNotes.length === 0 && (
+        <div className="col-span-full my-10 text-center text-gray-500">
+          No notes found. Click on the Add Note button to add a note.
+        </div>
+      )}
+    </div>
+  );
 }
